ef9ebb5c9447848574856a89c79a168b8bd5ab36
Ehnand A. Azucena <104221982+blank0810@users.noreply.github.com>
2025-04-10 18:42:06 +0800

    Update App.js (3)

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ export default function ComingSoon() {
                     }
                 };
                 img.onerror = () => {
-                    // Handle failed loads gracefully
                     loadedCount++;
                     if (loadedCount === totalImages) {
                         setBgLoaded(true);
@@ -29,7 +28,15 @@ export default function ComingSoon() {
             });
         };
 
-        const imageUrls = ['/images/1.svg', '/images/2.svg', '/images/3.svg', '/images/4.svg', '/images/5.svg', '/images/6.svg'];
+        const imageUrls = [
+            '/images/1.svg',
+            '/images/2.svg',
+            '/images/3.svg',
+            '/images/4.svg',
+            '/images/5.svg',
+            '/images/6.svg',
+            '/images/coming-soon.png', // Added to ensure it’s preloaded
+        ];
         preloadImages(imageUrls);
 
         const interval = setInterval(() => {
@@ -46,6 +53,7 @@ export default function ComingSoon() {
                 backgroundImage: `url(/images/${bgIndex}.svg)`,
             }}
         >
+            {!bgLoaded && <div className="loading">Loading...</div>}
             <div className='logo-wrapper'>
                 <img
                     src='/images/main-logo.png'
@@ -58,46 +66,49 @@ export default function ComingSoon() {
                     className='coming-soon-logo'
                 />
             </div>
+            {bgLoaded && (
+                <>
+                    <p className='sports-buddy-text'>- Your Sports Buddy -</p>
 
-            <p className='sports-buddy-text'>- Your Sports Buddy -</p>
+                    <p className='partner-text'>
+                        Partner with us. Email us at
+                        <a href='mailto:hello@playnow.ae' className='email-link'>
+                            {' '}
+                            hello@playnow.ae
+                        </a>
+                    </p>
 
-            <p className='partner-text'>
-                Partner with us. Email us at
-                <a href='mailto:hello@playnow.ae' className='email-link'>
-                    {' '}
-                    hello@playnow.ae
-                </a>
-            </p>
-
-            <div className='socials-container'>
-                <p className='socials-text'>
-                    Follow our socials and connect with us:
-                </p>
-            </div>
-            <div className='socials-wrapper'>
-                <a
-                    href='https://www.instagram.com/playnow.ae/'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    <img
-                        src='/images/instagram.png'
-                        alt='Instagram'
-                        className='social-icon'
-                    />
-                </a>
-                <a
-                    href='https://www.facebook.com/profile.php?id=61567354175981'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    <img
-                        src='/images/facebook.png'
-                        alt='Facebook'
-                        className='social-icon'
-                    />
-                </a>
-            </div>
+                    <div className='socials-container'>
+                        <p className='socials-text'>
+                            Follow our socials and connect with us:
+                        </p>
+                    </div>
+                    <div className='socials-wrapper'>
+                        <a
+                            href='https://www.instagram.com/playnow.ae/'
+                            target='_blank'
+                            rel='noopener noreferrer'
+                        >
+                            <img
+                                src='/images/instagram.png'
+                                alt='Instagram'
+                                className='social-icon'
+                            />
+                        </a>
+                        <a
+                            href='https://www.facebook.com/profile.php?id=61567354175981'
+                            target='_blank'
+                            rel='noopener noreferrer'
+                        >
+                            <img
+                                src='/images/facebook.png'
+                                alt='Facebook'
+                                className='social-icon'
+                            />
+                        </a>
+                    </div>
+                </>
+            )}
         </div>
     );
 }
