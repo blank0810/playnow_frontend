2c86d24d2cb9f4d3923470e2f0b78b98a2ddcb31
blank <azucenaehnand@gmail.com>
2025-04-16 16:34:20 +0800

    Coupon fix

--- components/couponListing/couponData.tsx
+++ components/couponListing/couponData.tsx
@@ -6,42 +6,48 @@ const CouponData: Coupon[] = [
     icon: "/images/icon/icon-03.svg",
     title: "Coupon",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
-    price: 0
+    price: 0,
+    category: 'Foods'
   },
   {
     id: 2,
     icon: "/images/icon/icon-03.svg",
     title: "Coupon",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
-    price: 0
+    price: 0,
+    category: 'Sports'
   },
   {
     id: 3,
     icon: "/images/icon/icon-03.svg",
     title: "Coupon",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
-    price: 0
+    price: 0,
+    category: 'Sports'
   },
   {
     id: 4,
     icon: "/images/icon/icon-03.svg",
     title: "Coupon",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
-    price: 0
+    price: 0,
+    category: 'Entertainment'
   },
   {
     id: 5,
     icon: "/images/icon/icon-03.svg",
     title: "Coupon",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
-    price: 0
+    price: 0,
+    category: 'Entertainment'
   },
   {
     id: 6,
     icon: "/images/icon/icon-03.svg",
     title: "Coupon",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
-    price: 0
+    price: 0,
+    category: 'Entertainment'
   },
 ];
 
